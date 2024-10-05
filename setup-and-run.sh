8ce775944b312a6166c4925717add92d85befd54
Saurav Shaw <sauravshaw@Sauravs-MacBook-Air.local>
2024-10-05 19:00:06 +0530

    added .sh script

--- setup-and-run.sh
+++ setup-and-run.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
 # Update package list
-sudo apt update
+apt update
 
 # Install required packages
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+apt install -y apt-transport-https ca-certificates curl software-properties-common
 
 # Add Docker's official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
 # Add the Docker repository
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 # Update package list again
-sudo apt update
+apt update
 
 # Install Docker
-sudo apt install -y docker-ce
+apt install -y docker-ce
 
 # Install Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 # Add current user to docker group
-sudo usermod -aG docker ${USER}
+usermod -aG docker ${USER}
 
 # Inform user about group changes
 echo "You have been added to the docker group. Please log out and log back in for this to take effect."
